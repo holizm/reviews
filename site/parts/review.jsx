a5c5f68fce3630f8ef9edf68d0330802bdce25f4
Saeed Nemati <sn.1361@gmail.com>
2026-08-23 14:51:18 +0330

    committed by script

--- site/parts/review.jsx
+++ site/parts/review.jsx
@@ -1,6 +1,6 @@
 export default ({ review }) => <article class='review'>
     <h2 class='title'>{review.title}</h2>
     <span class='reviewer'>{review.reviewer?.title}</span>
-    <time class='reviewDate'>{review.reviewDate}</time>
+    <time class='date'>{review.reviewDate}</time>
     <div class='content'>{review.content}</div>
 </article>
